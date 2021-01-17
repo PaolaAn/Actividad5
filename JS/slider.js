9b85c7716ea756f835ca81046323b64fd7f5c7d2
paolagonzalez <pgonza11@ibero.edu.co>
2021-01-17 14:45:44 -0500

    primer actividad en git

---
+++ JS/slider.js
@@ -0,0 +1,10 @@
+
+$('.jb').mouseover(function(){
+    $(this).css('background', 'plum').css('color', 'black')
+ });
+ 
+ $('.jb').mouseout(function(){
+    $(this).css('background', 'white').css('color', 'skyblue')
+ });
+
+
